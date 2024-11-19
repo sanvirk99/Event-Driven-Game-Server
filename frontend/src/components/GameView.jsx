87c896d13de32496cc64fc5c1978f5acdb6a760e
sanvirk99 <sanseerat99@gmail.com>
2024-11-18 16:16:49 -0800

    sync cards with game state and facedown card addition

--- frontend/src/components/GameView.jsx
+++ frontend/src/components/GameView.jsx
@@ -5,8 +5,11 @@ const imgPath = "/src/assets/img/";
 const cardValues = ['0', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'A'];
 const suits = ['D', 'H', 'S', 'C'];
 
-const getCardPath = (value, suit) => {
-    return `${imgPath}/${value}${suit}_python.svg`
+const getCardPath = (face, suit, cardUp = true) => {
+
+    let path = cardUp ?  `${imgPath}/${face}${suit}_python.svg` : `${imgPath}/back.svg`
+
+    return path
 };
 
 
@@ -19,8 +22,11 @@ const DisplayCards = () => {
               {suits.map((suit) => (
                 <img className="deck-card" src={getCardPath(value,suit)} key={`${value}${suit}`} />
               ))}
+
             </div>
           ))}
+
+           
         </div>
       );
 
@@ -75,7 +81,39 @@ const JoinGame = ({connection,myId}) => {
 const GameView=({connection,gameId,gameState,myId}) => {
 
 
+    const [dealer, setDealer] = useState(undefined)
+    const [players, setPlayers] = useState([])
+
     useEffect(() => {
+
+        if(gameId && gameState){
+
+            setDealer({
+                id: 'dealer',
+                cards: gameState.dealer.hand.cards,
+                count: gameState.dealer.hand.count
+            })
+            
+            if(!gameState.players){
+                return
+            }
+
+            console.log(Object.keys(gameState.players))
+
+            let copy = []
+            Object.keys(gameState.players).map((playerId) => {
+
+                copy.push({
+                    id: playerId,
+                    cards: gameState.players[playerId].hand.cards,
+                    count: gameState.players[playerId].hand.count
+                })
+
+            })
+
+            setPlayers(copy)
+
+        }
 
 
 
@@ -85,10 +123,19 @@ const GameView=({connection,gameId,gameState,myId}) => {
     return (
         <>
         <div>Game View
-        {(gameId ?  <pre>{JSON.stringify(gameState, null, 2)}</pre> : <JoinGame connection={connection} myId={myId}/> )}
+        {(gameId ?(
+            <div>
+                <div>Dealer: {dealer ? (<div className="deck-row"> {dealer.id} {dealer.count} {dealer.cards.map((card,index) => <img key={index} className="deck-card" src={getCardPath(card.face, card.suit,card.cardUp)} />)} </div>) : null}</div>
+                <div>Players: {players.map((player) => (<div className="deck-row" key={player.id}> {player.cards.map((card) => <img className="deck-card" src={getCardPath(card.face, card.suit)} />)} </div>))} </div>
+            </div>
+        ) : <JoinGame connection={connection} myId={myId}/>)}  
+              
+        {/* <pre>{JSON.stringify(gameState, null, 2)}</pre>  */}
+        
         </div>
 
-        <DisplayCards />
+        {/* <DisplayCards /> */}
+
         </>
         
     )
